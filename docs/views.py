38a4a63cbde6e2fdd09ce8ff965568906265b708
dwnsnu <dwnsnu@snu.ac.kr>
2023-06-03 02:17:26 +0900

    update: base64 to jpg

--- docs/views.py
+++ docs/views.py
@@ -5,14 +5,15 @@ from rest_framework import status
 
 # Create your views here.
 
-
+import base64
 class Index(APIView):
     def get(self, request, format=None):
         print('hi index in docs app')
         return Response(data='hello world get!', status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
-        # tmp = request['hello']
-        # print(request.data['image']['base64'])
-        print("data:image/jpg;base64,"+request.data['image']['base64'])
+        # print("data:image/jpg;base64,"+request.data['image']['base64'])
+        image_binary = base64.decodebytes(request.data['image']['base64'])
+        with open('img.jpg', 'w') as f:
+            f.write(image_binary)
         return Response(data='hello world post!', status=status.HTTP_200_OK)
